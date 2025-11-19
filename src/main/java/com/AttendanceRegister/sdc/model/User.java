2293ad4a7b8cb79f42f7aa0950cd8e51d591121c
mohaneswar123 <pvmohaneswar@gmail.com>
2025-11-19 15:42:48 +0530

    updated user modal

--- src/main/java/com/AttendanceRegister/sdc/model/User.java
+++ src/main/java/com/AttendanceRegister/sdc/model/User.java
@@ -1,6 +1,7 @@
 package com.AttendanceRegister.sdc.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDate;
 
@@ -9,9 +10,9 @@ public class User {
 
     @Id
     private String id;
-
+    @Indexed(unique = true)
     private String username;
-
+    @Indexed(unique = true)
     private String password;
 
     private String email;
--- src/main/java/com/AttendanceRegister/sdc/security/WebConfig.java
+++ src/main/java/com/AttendanceRegister/sdc/security/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // ✅ Correct: allow all routes
-                .allowedOrigins("https://attendanceinhand.netlify.app") // ✅ No trailing slash
+                .allowedOrigins("https://attendanceinhand.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
