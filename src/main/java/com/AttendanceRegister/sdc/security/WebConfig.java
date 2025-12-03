e2a548240103e331f58b46d1c8fa6dc25b209a2d
mohaneswar123 <pvmohaneswar@gmail.com>
2025-12-03 10:36:01 +0530

    making user active by default

--- src/main/java/com/AttendanceRegister/sdc/security/WebConfig.java
+++ src/main/java/com/AttendanceRegister/sdc/security/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // ✅ Correct: allow all routes
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://attendanceinhand.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
