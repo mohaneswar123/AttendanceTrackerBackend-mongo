d736d57d7455efcfcf7af0e1736d16c44ce452ef
mohaneswar123 <163732824+mohaneswar123@users.noreply.github.com>
2026-04-20 11:29:45 +0530

    Fix typo in print statement output

--- src/main/java/com/AttendanceRegister/sdc/AttendanceRegisterApplication.java
+++ src/main/java/com/AttendanceRegister/sdc/AttendanceRegisterApplication.java
@@ -1,4 +1,4 @@
-package com.AttendanceRegister.sdc;
+lpackage com.AttendanceRegister.sdc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +8,7 @@ public class AttendanceRegisterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AttendanceRegisterApplication.class, args);
-		System.out.println("runnnnnnnnnnn");
+		System.out.println("runnnnnnnnnnnn");
 	}
 
 }
